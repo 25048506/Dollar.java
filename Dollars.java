5432651a0390cffb3de13c791c253f7793cb0167
25048506 <25048506@mvula.univen.ac.za>
2025-03-10 19:23:53 +0200

    commit

---
+++ Dollars.java
@@ -0,0 +1,7 @@
+public class Dollars {
+    public static void main(String[] args) {
+        int dollars = 5;
+        int cents = 50;
+        System.out.println("I have " + dollars + " dollars and " + cents + " cents.");
+    }
+}
